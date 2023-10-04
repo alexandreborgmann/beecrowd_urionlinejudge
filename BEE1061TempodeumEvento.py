84f960adf96106af5054083f04bc5f1bdb8c0b51
Alexandre Borgmann <40210587+alexandreborgmann@users.noreply.github.com>
2023-10-04 11:49:25 -0300

    Add files via upload

---
+++ BEE1061TempodeumEvento.py
@@ -0,0 +1,12 @@
+s, diaInicio = map(str, input("").split())
+horaInicio,s1, minutoInicio, s2, segundoInicio = map(str, input("").split())
+s, diaFim = map(str, input("").split())
+horaFim,s1, minutoFim, s2, segundoFim = map(str, input("").split())
+dia = 0
+hora = 0
+minuto = 0
+segundo = 0
+print('{} dia(s)'.format(dia))
+print('{} hora(s)'.format(hora))
+print('{} minuto(s)'.format(minuto))
+print('{} segundo(s)'.format(segundo))
